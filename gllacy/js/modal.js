78b26d13daf5aee440898b54eca7229ae170e239
dmitriy-morozov <dimokin777@mail.ru>
2016-10-18 09:31:16 +0300

    d

--- gllacy/js/modal.js
+++ gllacy/js/modal.js
@@ -1,17 +1,9 @@
-window.onload= function() {
-	document.getElementById('toggler').onclick = function() {
-		openbox('box', this);
-		return false;
-	};
-};
 function openbox(id, toggler) {
 	var div = document.getElementById(id);
 	if(div.style.display == 'block') {
 		div.style.display = 'none';
-		toggler.innerHTML = 'Открыть';
 	}
 	else {
 		div.style.display = 'block';
-		toggler.innerHTML = 'Закрыть';
 	}
 }
