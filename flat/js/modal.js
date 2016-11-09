f2c0bc3860b734e6d2a6466a0aaba9dafc025cc8
dmitriy-morozov <dimokin777@mail.ru>
2016-11-09 16:40:08 +0200

    Flat

--- flat/js/modal.js
+++
@@ -1,9 +0,0 @@
-function openbox(id, toggler) {
-	var div = document.getElementById(id);
-	if(div.style.display == 'block') {
-		div.style.display = 'none';
-	}
-	else {
-		div.style.display = 'block';
-	}
-}
