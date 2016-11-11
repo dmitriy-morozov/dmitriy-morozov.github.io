4c67e758c9f2de56c9e3de7a56ce46110049a07c
dmitriy-morozov <dimokin777@mail.ru>
2016-11-11 17:43:50 +0200

    minify

--- pink/js/map.js
+++ pink/js/map.js
@@ -1,18 +1,1 @@
-
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 14,
-     disableDefaultUI: true,
-    zoomControl: true,
-    center: {lat: 59.93632276, lng: 30.32106467},
-    scrollwheel: false
-  });
-
-  var image = 'img/map-marker.png';
-  var beachMarker = new google.maps.Marker({
-    position: {lat: 59.93632276, lng: 30.32106467},
-    map: map,
-
-    icon: image
-  });
-}
+function initMap(){var e=new google.maps.Map(document.getElementById("map"),{zoom:14,disableDefaultUI:!0,zoomControl:!0,center:{lat:59.93632276,lng:30.32106467},scrollwheel:!1}),o="img/map-marker.png";new google.maps.Marker({position:{lat:59.93632276,lng:30.32106467},map:e,icon:o})}
