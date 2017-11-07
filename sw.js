8cfd7c4b714a5f713ce4116c6b4087fff5477256
morozov <morozov@quartsoft.com>
2017-11-07 12:03:42 +0200

    Test change path to files

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
 importScripts('sw-toolbox.js');
 
-toolbox.precache(["/","/assets/css/*","/assets/js/*","index.html"]);
+toolbox.precache(["/","index.html"]);
 toolbox.router.get('/*', toolbox.cacheFirst);
 
 
